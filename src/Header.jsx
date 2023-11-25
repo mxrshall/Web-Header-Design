421903c98fe286647cccf248fc7715c7cf4495c6
mxrshall <martin22.krajcovic@gmail.com>
2023-11-25 13:26:36 +0100

    Pridanie textu a fontu

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,8 +1,11 @@
+import HeaderText from "./HeaderText"
 
 function Header() {
   return (
     <div className="w-full h-[100vh] bg-[url('./images/background.gif')] bg-cover">
-        <div className="w-full h-[100vh] bg-black opacity-50"></div>
+        <div className="w-full h-[100vh] bg-black opacity-50 flex items-center justify-center">
+          <HeaderText/>
+        </div>
     </div>
   )
 }
---
+++ src/HeaderText.jsx
@@ -0,0 +1,19 @@
+function HeaderText() {
+    return (
+        <div className="w-full h-[100vh] flex items-center justify-center flex-col">
+            <div className="w-1/2 text-white font-ds">
+                <h1>Japan</h1>
+            </div>
+            <div className="w-1/2 flex justify-center items-center flex-row">
+                <div className="w-1/2 h-1 bg-white"></div>
+                <div className="w-3 h-3 rounded-full bg-white mx-2"></div>
+                <div className="w-1/2 h-1 bg-white"></div>
+            </div>
+            <div className="w-1/2 flex justify-end items-center flex-row text-white font-ds">
+                <h1>city of future</h1>
+            </div>
+        </div>
+    )
+  }
+  
+  export default HeaderText
