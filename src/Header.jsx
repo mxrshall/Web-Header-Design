cc72929eb52cbf6e5acced3b9be0bd4252015e21
mxrshall <martin22.krajcovic@gmail.com>
2023-11-25 12:39:32 +0100

    Pridanie noveho komponentu

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,7 +2,7 @@
 function Header() {
   return (
     <div className="w-full h-[100vh] bg-[url('./images/background.gif')] bg-cover">
-        
+        <div className="w-full h-[100vh] bg-black opacity-50"></div>
     </div>
   )
 }
