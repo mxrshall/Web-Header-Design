dd46e07a6b4c6717745e5b68499eb8f630aba320
mxrshall <martin22.krajcovic@gmail.com>
2023-11-27 01:13:30 +0100

    Vytvorenie prvej animacie

--- src/HeaderText.jsx
+++ src/HeaderText.jsx
@@ -1,3 +1,5 @@
+import { motion } from "framer-motion"
+
 function HeaderText() {
     return (
         <div className="w-full h-[100vh] flex items-center justify-center flex-col">
@@ -5,9 +7,14 @@ function HeaderText() {
                 <p className="text-white text-[16rem] font-ds">Japan</p>
             </div>
             <div className="w-1/2 flex justify-center items-center flex-row">
-                <div className="w-1/2 h-1 bg-white"></div>
-                <div className="w-2 h-2 rounded-full bg-white mx-1"></div>
-                <div className="w-1/2 h-1 bg-white"></div>
+                <div className="w-1/2 h-1 flex justify-end">
+                    <motion.div className="w-0 h-1 bg-white" animate={{width: "100%"}} transition={{duration: 1, delay: 0.3}}></motion.div>
+                </div>
+                <motion.div className="w-2 h-2 rounded-full bg-white mx-1" animate={{ scale: 1 }} initial={{scale: 0}} 
+                transition={{ type: 'spring' }}></motion.div>
+                <div className="w-1/2 h-1">
+                    <motion.div className="w-0 h-1 bg-white" animate={{width: "100%"}} transition={{duration: 1, delay: 0.3}}></motion.div>
+                </div>
             </div>
             <div className="w-1/2 flex justify-end items-center text-white text-2xl font-ds mp-5">
                 <p>city of future</p>
