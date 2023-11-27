d96fb44a292c286fd374f089abf947ae2dddf894
mxrshall <martin22.krajcovic@gmail.com>
2023-11-27 01:33:48 +0100

    Animacie pre text

--- src/HeaderText.jsx
+++ src/HeaderText.jsx
@@ -5,11 +5,11 @@ function HeaderText() {
     return (
         <div className="w-full h-[100vh] flex items-center justify-center flex-col">
             <div className="w-1/2 h-40 flex items-end text-white font-ds -mb-16 ml-5">
-                <p className="text-white text-[16rem] font-ds">Japan</p>
+                <motion.p className="text-white text-[16rem] font-ds" animate={{y: 0, opacity: 1}} initial={{y: 20, opacity: 0}} transition={{duration: 0.8, delay: 1}}>Japan</motion.p>
             </div>
             <Bar/>
-            <div className="w-1/2 flex justify-end items-center text-white text-2xl font-ds mp-5">
-                <p>city of future</p>
+            <div className="w-1/2 flex justify-end items-center mp-5">
+                <motion.p className="text-white text-2xl font-ds" animate={{y: 0, opacity: 1}} initial={{y: -20, opacity: 0}} transition={{duration: 0.8, delay: 1}}>city of future</motion.p>
             </div>
         </div>
     )
