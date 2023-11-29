2a5e77dbb5476e83a5ccede2407a7a79a11b10eb
mxrshall <martin22.krajcovic@gmail.com>
2023-11-29 16:31:59 +0100

    Pridanie linear prechodu a divu

--- src/Popis.jsx
+++ src/Popis.jsx
@@ -3,7 +3,10 @@ import { motion } from "framer-motion"
 function Popis() {
   return (
     <motion.div className="w-full h-[100vh] bg-[url('./images/background.png')] bg-cover">
-        <div className="w-full h-[100vh] bg-black bg-opacity-70 flex items-center justify-center">
+        <div className="w-full h-[100vh] bg-gradient-to-b from-black to-transparent">
+            <div className="w-3/5 h-[100vh] bg-black">
+
+            </div>
         </div>
     </motion.div>
   )
