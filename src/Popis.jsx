f27fbd17175e1d84aefe2651eaff4d0b0ccd5e91
mxrshall <martin22.krajcovic@gmail.com>
2023-11-30 01:43:15 +0100

    Pridanie textu a noveho fontu

--- src/Popis.jsx
+++ src/Popis.jsx
@@ -1,11 +1,14 @@
 import { motion } from "framer-motion"
+import PopisText from "./PopisText"
+import PopisZnak from "./PopisZnak"
 
 function Popis() {
   return (
     <motion.div className="w-full h-[100vh] bg-[url('./images/background.png')] bg-cover">
         <div className="w-full h-[100vh] bg-gradient-to-b from-black to-transparent">
             <div className="w-3/5 h-[100vh] bg-black">
-
+              <PopisZnak/>
+              <PopisText/>
             </div>
         </div>
     </motion.div>
---
+++ src/PopisZnak.jsx
@@ -0,0 +1,11 @@
+import { motion } from "framer-motion"
+
+function PopisZnak() {
+  return (
+    <motion.div className="text-white font-japan">
+        <p>Ahoj</p>
+    </motion.div>
+  )
+}
+
+export default PopisZnak
